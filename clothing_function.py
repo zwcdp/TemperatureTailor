ad5dc24555edcf2df92efba6035c2aff278f6638
Jess <tol.jess@gmail.com>
2019-01-27 02:08:28 -0500

    adding clothing function

--- clothing_function.py
+++ clothing_function.py
@@ -1,1 +1,19 @@
 
+
+def main():
+    clothing_function(42)
+
+
+def clothing_function(temperature):
+    clothing = ""
+    
+    if temperature < 46:
+        clothing = "wear heave coat."
+    if temperature > 46 and temperature < 69:
+        clothing = "wear fleece jacket or big sweater and scarf."
+    if temperature > 69:
+        clothing = "optional light jacket."
+    clothing = "enjoy the nice weather."
+    
+    print(clothing)
+    return clothing
