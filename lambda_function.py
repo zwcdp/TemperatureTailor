5ddeb884f410d729267b7c00e99878052c890e1f
Bryan1041 <Bryan1041@me.com>
2019-01-27 06:10:39 -0500

    Added actual location support. :)

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,7 @@
 """
-This is a Python template for Alexa to get you building skills (conversations) quickly.
-Adapted from: https://github.com/KeithGalli/Alexa-Python/blob/master/basic_template.py
-"""
+    This is a Python template for Alexa to get you building skills (conversations) quickly.
+    Adapted from: https://github.com/KeithGalli/Alexa-Python/blob/master/basic_template.py
+    """
 
 
 from __future__ import print_function
@@ -26,54 +26,53 @@ def build_speechlet_response(title, output, reprompt_text, should_end_session):
             'outputSpeech': {
                 'type': 'PlainText',
                 'text': reprompt_text
-            }
+        }
         },
-        'shouldEndSession': should_end_session
-    }
+    'shouldEndSession': should_end_session
+}
 
 def build_response(session_attributes, speechlet_response):
     return {
         'version': '1.0',
         'sessionAttributes': session_attributes,
         'response': speechlet_response
-    }
+}
 
 
 # --------------- Functions that control the skill's behavior ------------------
 def get_test_response():
     """ An example of a custom intent. Same structure as welcome message, just make sure to add this intent
-    in your alexa skill in order for it to work.
-    """
+        in your alexa skill in order for it to work.
+        """
     session_attributes = {}
     card_title = "Test"
     speech_output = "This is a test message"
     reprompt_text = "You never responded to the first test message. Sending another one."
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+                                                                       card_title, speech_output, reprompt_text, should_end_session))
 
 def get_welcome_response(context):
     """ If we wanted to initialize the session to have some attributes we could
-    add those here
-    """
+        add those here
+        """
     session_attributes = {}
     card_title = "Welcome"
     
     #speech_output = "Welcome to your custom alexa application!"
     
-
-    # Get ZIP code from Alexa Address API.
-    #deviceId = context.System.device.deviceId
-    #apiAccessToken = context.System.apiAccessToken
-    #countryAndPostalCode = requests.get("https://api.amazonalexa.com/v1/devices/" + deviceId + "/settings/address/countryAndPostalCode");
-    #zipCode = countryAndPostalCode.postalCode   # Stored as a string.
-
-    # Give ZIP to ZIPAPI and receive GPS coords.
-
+    # Get coordinates.
+    coordinates = requests.get("http://ip-api.com/json/")
+    latitude = coordinates.json()["lat"]
+    longitude = coordinates.json()["lon"]
+    
+    # Future feature: more personalized responses?
+    #city = coordinates.json()["city"]
+    
     # Add GPS coords in Dark Sky API call.
-    weather_output = requests.get("https://api.darksky.net/forecast/f3b60e3860754a0934693e5bbe7cf56e/37.8267,-122.4233")
+    weather_output = requests.get("https://api.darksky.net/forecast/f3b60e3860754a0934693e5bbe7cf56e/" + str(latitude) + "," + str(longitude))
     temperature = weather_output.json()["currently"]["temperature"]
-
+    
     if weather_output.json()["currently"]["summary"] == "Clear":
         condition = True
     else:
@@ -92,45 +91,44 @@ def get_welcome_response(context):
     # that is not understood, they will be prompted again with this text.
     reprompt_text = "I don't know if you heard me, welcome to your custom alexa application!"
     should_end_session = True;
-    return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session))
+    return build_response(session_attributes, build_speechlet_response(card_title, speech_output, reprompt_text, should_end_session))
 
 
 def handle_session_end_request():
     card_title = "Session Ended"
     speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
-                    "Have a nice day! "
+        "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
     return build_response({}, build_speechlet_response(
-        card_title, speech_output, None, should_end_session))
+                                                       card_title, speech_output, None, should_end_session))
 
 # --------------- Events ------------------
 
 def on_session_started(session_started_request, session):
     """ Called when the session starts.
-        One possible use of this function is to initialize specific 
+        One possible use of this function is to initialize specific
         variables from a previous state stored in an external database
-    """
+        """
     # Add additional code here as needed
     pass
 
-    
+
 
 def on_launch(launch_request, session, context):
     """ Called when the user launches the skill without specifying what they
-    want
-    """
+        want
+        """
     # Dispatch to your skill's launch message
     return get_welcome_response(context)
 
 
 def on_intent(intent_request, session):
     """ Called when the user specifies an intent for this skill """
-
+    
     intent = intent_request['intent']
     intent_name = intent_request['intent']['name']
-
+    
     # Dispatch to your skill's intent handlers
     if intent_name == "test":
         return get_test_response()
@@ -144,35 +142,35 @@ def on_intent(intent_request, session):
 
 def on_session_ended(session_ended_request, session):
     """ Called when the user ends the session.
-
-    Is not called when the skill returns should_end_session=true
-    """
+        
+        Is not called when the skill returns should_end_session=true
+        """
     print("on_session_ended requestId=" + session_ended_request['requestId'] +
           ", sessionId=" + session['sessionId'])
-    # add cleanup logic here
+# add cleanup logic here
 
 
 # --------------- Main handler ------------------
 
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
-    etc.) The JSON body of the request is provided in the event parameter.
-    """
+        etc.) The JSON body of the request is provided in the event parameter.
+        """
     print("Incoming request...")
-
+    
     """
-    Uncomment this if statement and populate with your skill's application ID to
-    prevent someone else from configuring a skill that sends requests to this
-    function.
-    """
+        Uncomment this if statement and populate with your skill's application ID to
+        prevent someone else from configuring a skill that sends requests to this
+        function.
+        """
     # if (event['session']['application']['applicationId'] !=
     #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
     #     raise ValueError("Invalid Application ID")
-
+    
     if event['session']['new']:
         on_session_started({'requestId': event['request']['requestId']},
                            event['session'])
-
+    
     if event['request']['type'] == "LaunchRequest":
         return on_launch(event['request'], event['session'], context)
     elif event['request']['type'] == "IntentRequest":
