c3695a8bf54fab3e705acb91f45765870e944daf
Jess <tol.jess@gmail.com>
2019-01-27 05:03:29 -0500

    Added support for precipitation and weather conditions. Need to adjust all calls.

--- clothing_function.py
+++ clothing_function.py
@@ -1,50 +1,49 @@
 # Function to determine what clothing to wear given the current temperature.
-def clothing_function(temperature):
+def clothing_function(temperature, raining, clear):
     
-    if sunny: 
+    if clear: 
         if temperature < 46:
-            clothing = " heavy coat."
-    if temperature > 46 and temperature < 69:
-        clothing = " fleece jacket or big sweater and scarf."
-    if temperature > 69:
-        clothing = "optional light jacket."
-   
-    if cloudy:
-        if temperature < 46:
-            clothing = " heavy coat and scarf."
-    if temperature > 46 and temperature < 69:
-        clothing = " fleece jacket or big sweater and scarf."
-    if temperature > 69:
-        clothing = "light jacket."
-    
-    else:
-        if sunny:
             if raining:
-                if temperature < 46:
-                    clothing = " heavy coat with umbrella."
-    if temperature > 46 and temperature < 69:
-        clothing = " rain jacket, sweater, and umbrella."
-    if temperature > 69:
-        clothing = "optional light jacket and umbrella."
-
-    if cloudy:
-        if raining:
-            if temperature < 46:
-                clothing = " heavy coat, scarf and umbrella."
-    if temperature > 46 and temperature < 69:
-        clothing = " fleece jacket or big sweater and scarf and don't forget the umbrella."
-    if temperature > 69:
-        clothing = "light jacket and umbrella."
-    
+                clothing = " a heavy coat with umbrella."
+            else:
+                clothing = " a heavy coat."
+        if temperature > 46 and temperature < 69:
+            if raining:
+                clothing = " a rain jacket, sweater, and umbrella."
+            else:
+                clothing = " a fleece jacket, or a big sweater and scarf."
+        if temperature > 69:
+            if raining:
+                clothing = " an umbrella and optional light jacket"
+            else:
+                clothing = " an optional light jacket."
+    else:
+        if temperature < 46:
+            if raining:
+                clothing = " a heavy coat, scarf, and umbrella"
+            else:
+                clothing = " a heavy coat and scarf."
+        if temperature > 46 and temperature < 69:
+            if raining:
+                clothing = " a fleece jacket, or a big sweater and scarf. Also, don't forget your umbrella."
+            else:
+                clothing = " a fleece jacket, or a big sweater and scarf."
+        if temperature > 69:
+            if raining:
+                clothing = " a light jacket and umbrella."
+            else:
+                clothing = " a light jacket."
 
     return clothing
 
 
 #def main():
-#    
-#    testTemperature = 42
+#   
+#    testTemperature = 68
+#    testRaining = False
+#    testClear = True
 #
-#    print(clothing_function(testTemperature))
+#    print(clothing_function(testTemperature, testRaining, testClear))
 #
 #main()
 
